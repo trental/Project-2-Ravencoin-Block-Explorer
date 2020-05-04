ca501860ae5a4421b365ee46df08cf077f54cf84
trental <trentlehmann@gmail.com>
2020-05-04 04:59:08 -0500

    Updates

---
+++ precache-manifest.978a4d1dc0a331835810acb75471f27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283631c3f8950f089ded439590d4e017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce340de1aebd70ee458",
+    "url": "/static/css/main.7043016a.chunk.css"
+  },
+  {
+    "revision": "65862a28afca69276891",
+    "url": "/static/js/2.acf0bdfe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.acf0bdfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce340de1aebd70ee458",
+    "url": "/static/js/main.ba611b9b.chunk.js"
+  },
+  {
+    "revision": "52cc8e5d112e411b7829",
+    "url": "/static/js/runtime-main.4b8e3499.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Project-2-Ravencoin-Block-Explorer/precache-manifest.66ef17f774dff60e2700caa984b59e65.js"
+  "/precache-manifest.978a4d1dc0a331835810acb75471f27b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Project-2-Ravencoin-Block-Explorer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
